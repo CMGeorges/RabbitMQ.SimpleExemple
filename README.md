6666aa836edfe5cb990cdeca224536cfee6b4ee9
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2021-02-24 22:37:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RabbitMQ.SimpleExemple
+ C# console Rabbit hello world
---
+++ RabbitMq.Producer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMq.Producer
+{
+    //cmd docker docker run --hostname my-rabbit --name ecomm-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("hello",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var message = new { Name = "Producer", Message = "Hello!" };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish("", "hello", null, body);
+            Console.ReadKey();
+        }
+    }
+}
